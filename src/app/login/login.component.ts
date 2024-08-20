06865eb67b30f6c5dc577ccdc29a86e66b8c0cac
izzhamburhan <maizzham@gmail.com>
2024-08-21 00:20:57 +0800

    feat : Login Page * created Google Identity Log In * designed using component (web search) * add component browse - to check after login success * routes changed * auth service added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+declare var google:any;
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  private router = inject(Router);
+  ngOnInit() {
+    google.accounts.id.initialize({
+      client_id: '908379805135-t0vgn7c1olfovrjprt7o80hu2a9ii6di.apps.googleusercontent.com',
+      callback: (resp: any)=> {
+        this.handleLogin(resp);
+      }
+    });
 
+    google.accounts.id.renderButton(document.getElementById("google-btn"), {
+      theme: 'filled_blue',
+      size: 'large',
+      shape: 'rectangle',
+      type: 'standard',
+      text: 'Sign in with Google'
+    })
+  }
+
+  private decodeToken(token: string) {
+    return JSON.parse(atob(token.split('.')[1]))
+  }
+
+  handleLogin(response: any) {
+    if(response) {
+      //decode the token
+      const payLoad = this.decodeToken(response.credential)
+      //store in session
+      sessionStorage.setItem('LoggedInUser', JSON.stringify(payLoad));
+      //navigate to home/browse
+      this.router.navigate(['browse']);
+    }else{
+      console.log('Login failed');
+    }
+  }
 }
